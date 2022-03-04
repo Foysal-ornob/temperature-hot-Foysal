dff3a70baba41e0b341d1c5fd973f0dbb8482d60
Foysal-ornob <fmd37304@gmail.com>
2022-03-04 14:03:13 +0300

    api key

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-const apiKay = `76bd4687e783eedf7cb4331fa69159e0`; 
+const apiKay = `76bd4687e783eedf7cb4331fa69159e0`;
+
+const searchTemperature = ()=>{
+    const city= document.getElementById('searcName').value;
+
+    const url=`http://api.openweathermap.org/data/2.5/weather?q={${city}}&appid={${apiKay}}`
+
+    console.log(url);
+
+}
